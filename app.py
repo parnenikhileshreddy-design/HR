674e974219ed06cd02308cab66aa468802151794
Nikhilreddy <parnenikhileshreddy@gmail.com>
2026-03-23 19:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,7 @@ def chat():
     return chatbot(q)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
